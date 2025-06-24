90a9c89e5b9da3309a45db77131b24682a5851b3
naol <naolmeseret22@gmail.com>
2025-06-24 17:38:32 +0300

    set and get and fetch and save the data

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-console.log("hello world");
+import { User } from "./models/User";
+
+const user = new User({});
+
+user.fetch();
---
+++ src/models/User.ts
@@ -0,0 +1,46 @@
+import axios, { AxiosResponse } from "axios";
+interface UserProp {
+  name?: string;
+  age?: number;
+}
+type CallBack = () => void;
+
+export class User {
+  events: { [key: string]: CallBack[] } = {};
+  constructor(private data: UserProp) {}
+  get(propName: string): number | string {
+    return this.data[propName];
+  }
+  set(update: UserProp): void {
+    Object.assign(this.data, update);
+  }
+  on(eventName: string, callback: CallBack) {
+    const handler = this.events[eventName] || [];
+    handler.push(callback);
+    this.events[eventName] = handler;
+  }
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+    if (!handlers || handlers.length === 0) {
+      return;
+    }
+    handlers.forEach((callback) => {
+      callback();
+    });
+  }
+  fetch(): void {
+    axios
+      .get(`http://localhost:3000/users/${this.get("id")}`)
+      .then((response: AxiosResponse) => {
+        this.set(response.data);
+      });
+  }
+  save(): void {
+    const id = this.get("id");
+    if (id) {
+      axios.put(`http://localhost:3000/users${id}`, this.data);
+    } else {
+      axios.post("http://localhost:3000/users", this.data);
+    }
+  }
+}
